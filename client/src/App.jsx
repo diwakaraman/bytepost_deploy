41ca29792c64cfa06ff9ce34d457b3dc56ab3326
AMAN DIWAKAR <154485677+diwakaraman@users.noreply.github.com>
2025-07-31 11:34:05 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
-import Footer from './components/Footer'; 
+import Footer from "./components/Footer";
+
 
 import Home from './pages/Home';
 import About from './pages/About';
